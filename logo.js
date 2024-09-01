c159cd3ab8b85bc4eaea1522abdb439598649674
David Marrs <david@marrsbytes.com>
2024-09-02 00:53:28 +0100

    Split out parser into its own function.

--- logo.js
+++ logo.js
@@ -2,13 +2,16 @@
 
 function initLogo(turtle) {
     return {
-        eval(script) {
+        parse(script) {
             var ast = [];
             var indent = null;
             for (var line of script.split("\n")) {
                 ast.push(line.split(/\s/));
             }
-            return turtle.perform(ast);
+            return ast;
+        },
+        eval(script) {
+            return turtle.perform(this.parse(script));
         }
     }
 }
