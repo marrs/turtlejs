0eb9f27945061085d6bb3e11c81f29fc51306db1
David Marrs <david@marrsbytes.com>
2024-09-03 00:44:50 +0100

    Add some more preset examples.
    Not all of these will work.

--- turtle-geometry.js
+++ turtle-geometry.js
@@ -8,4 +8,16 @@ window.tg = {
     FORWARD 100
     RIGHT 90
     FORWARD 100`,
+
+    '1.1.1b':
+`TO SQUARE
+    REPEAT 4
+        FORWARD 100
+        RIGHT 90`,
+
+    '1.1.1c':
+`TO SQUARE SIZE
+    REPEAT 4
+        FORWARD SIZE
+        RIGHT 90`,
 };
